ceac1dcda0320d3d5846a415d93c9014a85de575
Volodymyr Batsyk <volodymyrbatsyk@MacBook-Air-Volodymyr.local>
2023-01-13 00:21:41 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import './App.css';
+const tg = window.Telegram.WebApp
+function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close( )
+  }
+
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+export default App;
