551cf6b12e24f27a3acea8e27532bf6e75663454
Volodymyr Batsyk <volodymyrbatsyk@MacBook-Air-Volodymyr.local>
2023-01-13 16:58:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { useEffect } from 'react';
-import './App.css';
-const tg = window.Telegram.WebApp
+import { useEffect } from "react";
+import "./App.css";
+const tg = window.Telegram.WebApp;
 function App() {
-
   useEffect(() => {
-    tg.ready()
-  }, [])
+    tg.ready();
+  }, []);
 
   const onClose = () => {
-    tg.close( )
-  }
+    tg.close();
+  };
 
   return (
     <div className="App">
       <h2>Hello</h2>
+      <p>
+        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+        cillum sint consectetur cupidatat.{" "}
+      </p>
       <button onClick={onClose}>Close</button>
     </div>
   );
